c7a9032aa59cc73be814ceef6b934662fca08b41
Gianfranco Talocchino <gftalocchino@gmail.com>
2024-12-15 20:15:38 -0300

    sytle: crc: changes to make loops clear

--- src/crc.c
+++ src/crc.c
@@ -16,9 +16,9 @@ void crc_ieee_802_3_update(struct crc_ieee_802_3_ctx *ctx, const void *data, siz
 {
     const uint8_t *d = data;
 
-    while(len--) {
-        ctx->state ^= *d++;
-        for(uint32_t bit = 0; bit < 8; bit++) {
+    for (size_t i = 0; i < len; i++) {
+        ctx->state ^= d[i];
+        for(uint32_t j = 0; j < 8; j++) {
             if ((ctx->state & 1u) != 0) {
                 ctx->state = (ctx->state >> 1u) ^ 0xedb88320;
             } else {
@@ -45,8 +45,8 @@ void crc_modbus_update(struct crc_modbus_ctx *ctx, const void *data, size_t len)
     for (size_t i = 0; i < len; i++) {
         ctx->state ^= d[i];
         for (size_t j = 0; j < 8; j++) {
-            if ((ctx->state & 0x0001) != 0) {
-                ctx->state = (ctx->state >> 1) ^ 0xa001;
+            if ((ctx->state & 1u) != 0) {
+                ctx->state = (ctx->state >> 1u) ^ 0xa001;
             } else {
                 ctx->state >>= 1;
             }
