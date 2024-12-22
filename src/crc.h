5a5dee16e0260740186abe122b27abab611ce94e
Gianfranco Talocchino <gftalocchino@gmail.com>
2024-12-22 20:21:03 -0300

    feat: crc: added new update functions based on luts

--- src/crc.h
+++ src/crc.h
@@ -45,6 +45,19 @@ void crc_ieee_802_3_init(struct crc_ieee_802_3_ctx *ctx);
 void crc_ieee_802_3_update(struct crc_ieee_802_3_ctx *ctx, const void *data, size_t len);
 
 /**
+ * @brief Updates the IEEE 802.3 CRC calculation with new data.
+ * 
+ * This function differs from crc_ieee_802_3_update in that it 
+ * is implemented based on a table. This improves speed at the 
+ * cost of using more program memory.
+ *
+ * @param ctx Pointer to the IEEE 802.3 CRC context structure.
+ * @param data Pointer to the data to process.
+ * @param len Length of the data in bytes.
+ */
+void crc_ieee_802_3_update_fast(struct crc_ieee_802_3_ctx *ctx, const void *data, size_t len);
+
+/**
  * @brief Finalizes the IEEE 802.3 CRC calculation.
  *
  * @param ctx Pointer to the IEEE 802.3 CRC context structure.
@@ -67,6 +80,19 @@ void crc_modbus_init(struct crc_modbus_ctx *ctx);
  * @param len Length of the data in bytes.
  */
 void crc_modbus_update(struct crc_modbus_ctx *ctx, const void *data, size_t len);
+
+/**
+ * @brief Updates the Modbus CRC calculation with new data.
+ *
+ * This function differs from crc_modbus_update in that it 
+ * is implemented based on a table. This improves speed at the 
+ * cost of using more program memory.
+ * 
+ * @param ctx Pointer to the Modbus CRC context structure.
+ * @param data Pointer to the data to process.
+ * @param len Length of the data in bytes.
+ */
+void crc_modbus_update_fast(struct crc_modbus_ctx *ctx, const void *data, size_t len);
 
 /**
  * @brief Finalizes the Modbus CRC calculation.
